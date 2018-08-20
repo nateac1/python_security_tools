3dd161e69682daa594ed3cca41170d36702f93a3
Apix <Apix@localhost.localdomain>
2018-08-20 12:40:45 -0500

    Added Log_Checker.py and README.md
     On branch master
    	new file:   Log_Checker.py
    	new file:   README.md

---
+++ Log_Checker.py
@@ -0,0 +1,33 @@
+"""
+This program looks through a log file and returns all lines that match the keyword.
+"""
+import os
+
+def logcheck(keyword,
+            filepath='/var/log/syslog',
+            outpath='/root/Desktop/logoutput'):
+    """
+    keyword:
+        Enter a word to search in the log.
+    filepath:
+        Enter the path to the log file; it can be absolute or relative.
+    outpath:
+        Enter the path to the output file.
+    """
+
+    # Open the log file and read the lines, then make a list to write to the new file
+    file = open(filepath,'r').readlines()
+    write = []
+    # Take each line and see if the keyword is in it; if so, then write the line to the text file.
+    for i in file:
+        if keyword in i:
+            write.append(i)
+
+    fileb = open(outpath, 'w')
+    fileb.writelines(write)
+    # Close the text file and return a message that states that the process is over.
+    fileb.close()
+    print('All done.')
+
+# call the function with 'USB' as the keyword.
+logcheck('usb', filepath='/var/log/syslog', outpath='/root/Desktop/Python Projects/Python for Pentesters/logoutput.txt')
---
+++ README.md
@@ -0,0 +1,3 @@
+This is a collection of scripts for practicing python development.
+
+My goal is to make something useful while I learn; these scripts should deem useful for pentesting and security work.
