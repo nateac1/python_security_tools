dc6b67304501347be75775403d81a0066674c844
Nathan Christensen <natchristensen1@gmail.com>
2020-03-07 17:51:51 -0600

    Update root.py

--- root.py
+++ root.py
@@ -1,7 +1,4 @@
-from pathlib import Path
-
-p = Path('/root/root.txt')
-with open(p, 'r') as f:
+with open('/root/root.txt', 'r') as f:
     with open('root.txt', 'w') as r:
         r.write(f.readlines()[0])
         r.close()
